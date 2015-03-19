3cde7767e8623e8717c8b6aa24a67317ba5eb8b9
Al Cattell <alcattell@iMac1-Well-Studio.local>
2015-03-19 14:44:35 +0000

    connected Mongo, removed autopublish

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,21 +1,6 @@
-var postsData = [
-	{
-		title: 'Introducing Telescope',
-		url: 'http://sachagrief.com/introducing-telescope/'
-	},
-	{
-		title: 'Meteor',
-		url: 'http://meteor.com'
-	},
-	{
-		title: 'The Meteor Book',
-		url: 'http://themeteorbook.com'
-	},
-	{
-		title: 'BBC',
-		url: 'http://www.bbc.co.uk'
-	}
-];
+
 Template.postsList.helpers({
-	posts: postsData
+	posts: function() {
+		return Posts.find();
+	}
 });
